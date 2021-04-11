1e9e34117cb51282558a289a343feffaab807c6c
shaoukii <sallemishaouki53@gmail.com>
2021-04-11 16:51:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (  
+    <div className="box">
+      <div className="container-fluide"> 
+      <div className="row">
+        <div className="col-sm">
+        <img src="re.png" alt="mdo" width="80" height="80"></img> 
+
+        </div>
+        <div className="col-sm">
+<h1>
+
+<h3>react.JS </h3>
+</h1>
+
+        </div>
+
+
+      </div>
+      
+      </div>
+      
+
+    </div>
+   
+    // <div classNameName="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
